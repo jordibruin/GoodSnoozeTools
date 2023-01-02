4ff86cfd6121a693f7751115834f29687d51953e
Jordi Bruin <jordi@goodsnooze.com>
2023-01-02 12:32:54 +0100

    fixed duplicate

--- Sources/GoodSnoozeTools/GoodSnoozeTools.swift
+++ Sources/GoodSnoozeTools/GoodSnoozeTools.swift
@@ -86,33 +86,6 @@ fileprivate extension Color {
     }
 }
 
-extension Color: Codable {
-    enum CodingKeys: String, CodingKey {
-        case red, green, blue
-    }
-    
-    public init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        let r = try container.decode(Double.self, forKey: .red)
-        let g = try container.decode(Double.self, forKey: .green)
-        let b = try container.decode(Double.self, forKey: .blue)
-        
-        self.init(red: r, green: g, blue: b)
-    }
-
-    public func encode(to encoder: Encoder) throws {
-        guard let colorComponents = self.colorComponents else {
-            return
-        }
-        
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        
-        try container.encode(colorComponents.red, forKey: .red)
-        try container.encode(colorComponents.green, forKey: .green)
-        try container.encode(colorComponents.blue, forKey: .blue)
-    }
-}
-
 // Use Hex as colors
 extension Color {
     init(hex: String) {
